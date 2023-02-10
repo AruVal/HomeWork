c4d69f53530c37d7a9475155a59b1238347ee57f
Aru <hotjelousy@gmail.com>
2023-02-11 00:45:30 +0300

    fix function

--- week10/week10.js
+++ week10/week10.js
@@ -1,12 +1,12 @@
 function defineYear(year) {
     if ((year % 4 === 0 && year % 100 !== 0) || (year % 100 === 0 && year % 400 === 0)) {
-        return year;
+        return true;
         } else {
         return false;
         }
     }
-
-
+let x = defineYear(1756);
+console.log(x);
 
 
 
