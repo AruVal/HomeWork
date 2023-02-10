8fc3651ea69f6b151762cef5a51a578f4dce5d12
Aru <hotjelousy@gmail.com>
2023-02-10 22:15:29 +0300

    add homework

--- week10/week10.js
+++ week10/week10.js
@@ -1,43 +1,13 @@
-let showMessage = () => console.log("Я учу JavaScript!");
-showMessage();
-
-/* Устанавливаем стартовый индекс слайда по умолчанию: */
-let slideIndex = 1;
-/* Вызываем функцию, которая реализована ниже: */
-showSlides(slideIndex);
-
-/* Увеличиваем индекс на 1 — показываем следующий слайд: */
-function nextSlide() {
-    showSlides(slideIndex += 1);
-}
-
-/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
-function previousSlide() {
-    showSlides(slideIndex -= 1);
-}
-
-/* Устанавливаем текущий слайд: */
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-/* Функция перелистывания: */
-function showSlides(n) {
-    /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
-    let slides = document.getElementsByClassName("item");
-
-    /* Проверяем количество слайдов: */
-    if (n > slides.length) {
-    slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
+function defineYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 100 === 0 && year % 400 === 0)) {
+        return year;
+        } else {
+        return false;
+        }
     }
 
-    /* Проходим по каждому слайду в цикле for: */
-    for (let slide of slides) {
-        slide.style.display = "none";
-    }
-    /* Делаем элемент блочным: */
-    slides[slideIndex - 1].style.display = "block";
-}
+
+
+
+
+
