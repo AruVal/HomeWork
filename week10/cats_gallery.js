df334135ae7396849b14995c2697a574d117bf4d
Aru <hotjelousy@gmail.com>
2023-07-17 19:30:32 +0300

    cats gallery

---
+++ week10/cats_gallery.js
@@ -0,0 +1,14 @@
+//Задание 1.
+let showMessage = () => console.log("Я учу JavaScript!");
+showMessage();
+
+//Задание 2.
+
+function next() {
+    document.getElementById('first').src="https://www.animalfunfacts.net/images/stories/pets/cats/abyssinian_cat_eyes_l.jpg";
+};
+
+
+function previous() {
+    document.getElementById('first').src="https://d3544la1u8djza.cloudfront.net/APHI/Blog/2020/09-24/About+Abyssinians+Appearance+Personality+and+Health+_+ASPCA+Pet+Health+Insurance+_+Abyssinian+cat+resting+atop+a+cat+tree-min.jpg";
+};
